1f652dcff8927a8b8679d1d396b9eafecaa20a66
jjyoung <blueguppy01@gmail.com>
2018-01-15 19:33:56 +0900

    test

--- src/main/java/peekaf/springprogramming/chap07/HomeController.java
+++ src/main/java/peekaf/springprogramming/chap07/HomeController.java
@@ -36,11 +36,9 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping(value = "/top", method = RequestMethod.GET)
 	public String top(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
